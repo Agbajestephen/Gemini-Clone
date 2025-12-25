7070e4966a7f9f1ff420895cd5c1d2e9ca886dfe
Agbajestephen <agbajestephen5@gmail.com>
2025-12-25 20:39:04 +0100

    Update Gemini.js

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -20,6 +20,7 @@ async function runChat(prompt) {
     maxoOutputToken: 2048,
   };
     {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
     {
       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
     },
