841f6e88aea76f716c58bf1473ea818636106eb1
Agbajestephen <agbajestephen5@gmail.com>
2026-01-01 19:52:31 +0100

    Update context.jsx

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,3 +1,35 @@
 import { createContext } from "react";
 
-const Context = createContext();
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSet = async (prompt) => {
+    await runChat(prompt);
+  };
+
+  onSet("Hello, how are you?");
+
+  const contextvalue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSet,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput
+  };
+
+  return (
+    <Context.Provider value={contextvalue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
