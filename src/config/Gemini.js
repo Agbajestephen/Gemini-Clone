b7efcf81a565c3f421b42ee02877cb389bb804b5
Agbajestephen <agbajestephen5@gmail.com>
2026-01-01 19:48:06 +0100

    Update Gemini.js

--- src/config/Gemini.js
+++ src/config/Gemini.js
@@ -2,7 +2,7 @@ import {
   GoogleGenerativeAI,
   HarmCategory,
   HarmBlockThreshold,
-} = require("@google/generative-ai");
+} from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-2.0-pro";
 const API_KEY = "AIzaSyCQcjJDUfMp3tyk4A1nqzciQoE-WcLQxIs";
